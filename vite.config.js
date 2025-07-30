164c91d715a9691f0b42038d41edabbaeabbc7ba
Bojan Jeftic <bojan_jeftic@outlook.com>
2025-07-30 23:47:58 +0200

    Initialization of frontend

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
@@ -11,7 +10,6 @@ export default defineConfig({
             refresh: true,
         }),
         vue(),
-        tailwindcss(),
     ],
     resolve: {
         alias: {
