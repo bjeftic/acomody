e41742db7b99d52cb93da82402bfa4b905ec38ed
Bojan <bojan_jeftic@outlook.com>
2026-01-28 20:59:43 +0000

    Search feature

---
+++ lang/en/filters.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'price' => 'Price',
+    'accommodation_occupation' => 'Type of place',
+    'accommodation_type' => 'Property type',
+    'amenities' => 'Amenities',
+
+];
