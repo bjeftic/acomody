a1fbee6eae995f99d35e3f32adf6460f542dd66d
Bojan <bojan_jeftic.com>
2026-03-10 23:29:48 +0100

    Bed type translations

--- app/Enums/Accommodation/BedType.php
+++ app/Enums/Accommodation/BedType.php
@@ -15,30 +15,12 @@ enum BedType: string
 
     public function label(): string
     {
-        return match ($this) {
-            self::Double => 'Double Bed',
-            self::King => 'King Bed',
-            self::Queen => 'Queen Bed',
-            self::Single => 'Single Bed',
-            self::Twin => 'Twin Bed',
-            self::Bunk => 'Bunk Bed',
-            self::Sofa => 'Sofa Bed',
-            self::Futon => 'Futon Mat',
-        };
+        return __('enums/bed_type.'.$this->value);
     }
 
     public function description(): string
     {
-        return match ($this) {
-            self::Double => '131–150 cm (52–59 inches) wide',
-            self::King => '181–210 cm (71–82 inches) wide',
-            self::Queen => '151–180 cm (60–70 inches) wide',
-            self::Single => '90–130 cm (35–51 inches) wide',
-            self::Twin => '90–130 cm (35–51 inches) wide',
-            self::Bunk => '',
-            self::Sofa => '',
-            self::Futon => '',
-        };
+        return __('enums/bed_type.'.$this->value.'_description');
     }
 
     public function toArray(): array
