ae563f5cd6d478857af23af44f3289f159f610ab
Bojan <bojan_jeftic.com>
2026-04-06 08:45:09 +0200

    horizon fix

--- config/horizon.php
+++ config/horizon.php
@@ -247,6 +247,37 @@ return [
             ],
         ],
 
+        'staging' => [
+            'supervisor-1' => [
+                'connection' => 'redis',
+                'queue' => ['default'],
+                'balance' => 'auto',
+                'autoScalingStrategy' => 'time',
+                'maxProcesses' => 3,
+                'maxTime' => 0,
+                'maxJobs' => 0,
+                'memory' => 128,
+                'tries' => 3,
+                'timeout' => 90,
+                'nice' => 0,
+            ],
+            'supervisor-photos' => [
+                'connection' => 'redis',
+                'queue' => ['photo-processing'],
+                'balance' => 'auto',
+                'autoScalingStrategy' => 'time',
+                'maxProcesses' => 2,
+                'maxTime' => 0,
+                'maxJobs' => 0,
+                'memory' => 256,
+                'tries' => 3,
+                'timeout' => 120,
+                'nice' => 0,
+                'balanceMaxShift' => 1,
+                'balanceCooldown' => 3,
+            ],
+        ],
+
         'local' => [
             'supervisor-1' => [
                 'connection' => 'redis',
